03affb5a53691e832b07f3da7d0d3c1fab5ae3ef
Emmanuel Oke <adunolafoyinsola@gmail.com>
2023-03-18 23:13:45 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints a specific message
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
