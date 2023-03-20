7ba58fac59cbb336a068debb32ae08c8ffdb12f9
Emmanuel Oke <adunolafoyinsola@gmail.com>
2023-03-20 07:41:37 +0000

    Inventing is a combination of braind and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints number combinations
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+
+				if (i != '8' || (i == '8' && j != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
