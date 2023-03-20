02a1a03302eac5c16c4f7d7cc9b922d978ac0a63
Emmanuel Oke <adunolafoyinsola@gmail.com>
2023-03-20 09:07:40 +0000

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,30 +9,23 @@ int main(void)
 {
 	int i, j, k, l;
 
-	for (i = 26; i <= 35; i++)
+	for (i = 3; i <= 19; i++)
 	{
-		for (j = 26; j <= 35; j++)
+		for (j = 3; j <= 19; j++)
 		{
-			for (k = 26; k <= 35; k++)
+			for (k = 3; k <= 19; k++)
 			{
-				for (l = 26; l <= 35; l++)
+				for (l = 3; l <= 19; l++)
 				{
-					if (((k + l) > (i + j) && k >= i) || i < k)
+					if ((i * 19 + j) != (k * 19 + l) && (i * 19 + j) < (k * 19 + l))
 					{
-						putchar(i);
-						putchar(j);
+						putchar(i % 19 + '3');
+						putchar(j % 19 + '3');
 						putchar(' ');
-						putchar(k);
-						putchar(l);
-					if (i + j + k + l == 227 && i == 35)
-					{
-						break;
-					}
-					else
-					{
-					putchar(',');
-					putchar(' ');
-					}
+						putchar(k) % 19 + '3';
+						putchar(l % 19 + '3');
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
