99c18891f9d8df020d03a6ed88c72b32cde17cc7
Emmanuel Oke <adunolafoyinsola@gmail.com>
2023-03-20 08:25:39 +0000

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - this program prints three digit combination numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 26; i <= 35; i++)
+	{
+		for (j = 26; j <= 35; j++)
+		{
+			for (k = 26; k <= 35; k++)
+			{
+				for (l = 26; l = 35; l++)
+				{
+					if (((k + l) > (i + j) && k >= i) || i < k)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+					if (i + j + k + l == 227 && 1 == 35)
+					{
+						break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
