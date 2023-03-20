b3b094c1c2943dba75b71b9bbbce442adc631635
Emmanuel Oke <adunolafoyinsola@gmail.com>
2023-03-20 07:23:09 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints number comnbinations of single digits
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
