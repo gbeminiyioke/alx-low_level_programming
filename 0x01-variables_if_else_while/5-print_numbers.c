18ba1d2d1ac2c87b36fa16de377b26ea0549d4b4
Emmanuel Oke <adunolafoyinsola@gmail.com>
2023-03-20 06:49:26 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints all single digit numbers from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
