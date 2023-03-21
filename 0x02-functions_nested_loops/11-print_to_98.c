ceeefaa3a3cf0aa92879bacaba1755239bae1466
Emmanuel Oke <adunolafoyinsola@gmail.com>
2023-03-21 22:26:15 +0000

    Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - this function prints all natural numbers
+ * from n to 98 followed by a new line
+ * @n: input number
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+				n--;
+			}
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+
+	printf("\n");
+}
