57af6f88919adf64be84537cebd57bf54a67f7ee
Emmanuel Oke <adunolafoyinsola@gmail.com>
2023-03-21 21:42:00 +0000

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - this function adds two integers
+ * @i: first integer input
+ * @j:  second integer input
+ * Return: returns the sum of i and j
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+	return (k);
+}
