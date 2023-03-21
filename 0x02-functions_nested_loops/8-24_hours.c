2c860299435c0179d62b030bcb97fdbbef254b36
Emmanuel Oke <adunolafoyinsola@gmail.com>
2023-03-21 20:44:35 +0000

    Im a federal Agent Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - this function prints every minute of the day
+ * of jack bauer
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+		}
+		i++;
+	}
+}
