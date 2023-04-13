0748b2c0ee421a54328e558f16964f0ca57a2169
gbeminiyioke <adunolafoyinsola@gmail.com>
2023-04-14 00:33:21 +0100

    updated folder

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * malloc_checked - this function allocates
+ * memory using malloc
+ * @b: butes yo allocate
+ * Return: pointer to allocated memory
+ */
+
