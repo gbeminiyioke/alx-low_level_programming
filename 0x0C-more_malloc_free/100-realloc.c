39893730842860907524de8a1669fea080591cae
gbeminiyioke <adunolafoyinsola@gmail.com>
2023-04-17 04:50:21 +0100

    update 100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,7 +3,7 @@
 /**
  * _realloc - this function reallocates memory
  * block using malloc and free
- * @ptr:
+ * @ptr: pointer
  * @old_size: size of the allocated memory
  * @new_size: size of the new memort block
  * Return: pointer to new
