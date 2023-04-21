3eb6a4cb3d8f14acd3519f3759e640d0006e74d7
gbeminiyioke <adunolafoyinsola@gmail.com>
2023-04-21 03:22:15 +0100

    Beauty is a variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,7 +5,7 @@
  * sum_them_all - this function returns the sum
  * of all its parameters
  * @n: number of parameters
- * @... variable number of parameters
+ * @...: variable number of parameters
  * Return: 0 or the sum of all tje parameters
  */
 
