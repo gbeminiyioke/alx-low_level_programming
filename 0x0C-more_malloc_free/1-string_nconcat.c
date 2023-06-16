c204415c328abdda955a373ca6271e64b021faae
gbeminiyioke <adunolafoyinsola@gmail.com>
2023-06-16 02:17:25 +0100

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,26 +11,29 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *c;
-	unsigned int a1 = 0, a2 = 0, a3 = 0;
 
-	if (s1 == NULL)
-		a1 = 0;
+	char *c;
+	unsigned int a1 = 0, a2 = 0, a3 = 0, a4 = 0;
+
+	while (s1 && s1[a3])
+		a3++;
+	while (s2 && s2[a4])
+		a4++;
+	if (n < a4)
+		c = malloc(sizeof(char) * (a3 + n + 1));
 	else
-		for (a1 = 0; s1[a1]; a1++)
-	if (s2 == NULL)
-		a2 = 0;
-	else
-		for (a2 = 0; s2[a2]; a2++)
-	if (a2 > n)
-		a2 = n;
-	c = malloc(sizeof(char) * (a1 + a2 + 1));
-	if (c == NULL)
+		c = malloc(sizeof(char) * (a3 + a4 + 1));
+	if (!c)
 		return (NULL);
-	for (a3 = 0; a3 < a1; a3++)
-		c[a3] = s1[a3];
-	for (a3 = 0; a3 < a2; a3++)
-		c[a3 + a1] = s2[a1];
-	c[a1 + a2] = '\0';
+	while (a1 < a3)
+	{
+		c[a1] = s1[a1];
+		a1++;
+	}
+	while (n < a4 && a1 < (a3 + n))
+		c[a1++] = s2[a2++];
+	while (n >= a4 && a1 < (a3 + a4))
+		c[a1++] = s2[a2++];
+	c[a1] = '\0';
 	return (c);
 }
