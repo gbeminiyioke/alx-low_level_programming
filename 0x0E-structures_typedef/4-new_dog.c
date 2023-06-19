427c1f1465b9ae9955057c621e6f5850118708be
gbeminiyioke <adunolafoyinsola@gmail.com>
2023-06-19 20:00:28 +0100

    A door is what a dog is

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,81 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *_strcpy - this function copies a string
+ * @dest: copy string to
+ * @src: string to copy
+ * Return: pointer dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = '\0';
+	}
+	dest[j]  = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - this function returns the lenght
+ * of a string
+ * @s: input string
+ * Return: lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * new_dog - this function creates a new dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ * Return: pointer to the new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int i, j;
+
+	i = _strlen(name);
+	j = _strlen(owner);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(sizeof(char) * (i + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (j + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+	return (dog);
+}
