f610216d498841f7905990a66e842f9e177730f1
gbeminiyioke <adunolafoyinsola@gmail.com>
2023-06-21 11:55:12 +0100

    to hell with circumstances

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - this function searches for an
+ * integer
+ * @array: array
+ * @size: size of the array
+ * @cmp: pointer to the function to be used
+ * Return: 0 or -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
