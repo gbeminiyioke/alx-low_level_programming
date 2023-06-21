e1769b6aaced04dcbf41039ccbad2c1d8480ca15
gbeminiyioke <adunolafoyinsola@gmail.com>
2023-06-21 14:02:57 +0100

    3-main.c update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,31 +13,27 @@
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1, num2; //declare int variables
-	char *op; //pointer to char
+	int int1, int2;
+	char *c;
 
 	if (argc != 4)
 	{
-		//print error & exit with 98
 		printf("Error\n");
 		exit(98);
 	}
-	//convert operator string to int
-	num1 = atoi(argv[1]);
-	op = argv[2];
-	num2 = atoi(argv[3]);
-	if (op[1] != '\0' || get_op_func(op) == NULL)
+	int1 = atoi(argv[1]);
+	c = argv[2];
+	int2 = atoi(argv[3]);
+	if (c[1] != '\0' || get_op_func(c) == NULL)
 	{
-		//if operator is not valid
-		//exit with status 99.
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	if ((*c == '/' && int2 == 0) || (*c == '%' && int2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n", get_op_func(c)(int1, int2));
 	return (0);
 }
