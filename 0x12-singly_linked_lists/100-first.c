a0843df21e5d5352e822848f153bb29ee090a988
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2023-07-03 20:28:15 +0000

    the hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void print_string(void) __attribute__ ((constructor));
+
+/**
+ * print_string - this function prints a string before the main function
+ */
+
+void print_string(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
