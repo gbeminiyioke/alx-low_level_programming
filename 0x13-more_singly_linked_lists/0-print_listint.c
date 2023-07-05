37313645f3068217b7e9454f783946fece7b5eee
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2023-07-05 16:56:05 +0000

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t list;
+	size_t lst;
 
-	list = 0;
+	lst = 0;
 	while (h)
 	{
 		printf("%d\n", h->n);
-		list++;
+		lst++;
 		h = h->next;
 	}
-	return (list);
+	return (lst);
 }
