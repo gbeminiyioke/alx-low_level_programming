160a725f19c3c1461781688a055916ffde94d77f
Gbeminiyi Oke <adunolafoyinsola@gmail.com>
2023-07-12 02:57:48 +0000

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 	do {
 		if (start == -1 || f_name == -1)
 		{
-			dprintf(STDERR_FILENO, "ErrorL Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		outp = write(end, buff, f_name);
